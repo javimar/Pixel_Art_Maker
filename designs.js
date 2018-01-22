808da0c56b0fa4d4cfc94e4e289cb061d427823a
javimar <javimardeveloper@gmail.com>
2018-01-22 11:50:24 +0100

    Add javascript logic and some html fixes

--- designs.js
+++ designs.js
@@ -1,10 +1,95 @@
-// Select color input
-// Select size input
+// Globals constants are declared with "const" and capitalized, if they were to change, use "let"
+
+// Secure the table
+const CANVAS_GRID_TABLE = $("#pixel_canvas");
+
+// color
+let COLOR_PICKER = $("#colorPicker").val();
+
+// Secure the inputs for height and width
+const INPUT_HEIGHT = $("#input_height");
+const INPUT_WIDTH = $("#input_width");
+
+// Secure the submit button
+const SUBMIT_BUTTON = $('input[type="submit"]');
+
+let mouseDrag;
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid()
+{
+  // Set CANVAS_GRID_TABLE size as N by M grid
+  let gridHeight = INPUT_HEIGHT.val();
+  let gridWidth = INPUT_WIDTH.val();
 
-function makeGrid() {
+  // Clear the CANVAS_GRID_TABLE first
+  CANVAS_GRID_TABLE.empty();
 
-// Your code goes here!
+  // rows
+  for (let i = 0; i < gridHeight; i++)
+  {
+    const cell = $("<tr></tr>");
+    CANVAS_GRID_TABLE.append(cell);
 
-}
+    // cols
+    for (let j = 0; j < gridWidth; j++)
+    {
+      cell.append("<td></td>");
+    }
+  }
+
+  //Select color input
+  $("td").on("click", function()
+  {
+    const color = $("#colorPicker").val();
+    $(this).css("background-color", color);
+  });
+} // END OF FUNCTION makeGrid()
+
+
+// Create table of grids when "Make Grid" is clicked
+$("#makeGrid").on("click", function(event)
+{
+  makeGrid();
+  event.preventDefault();
+});
+
+
+// Clear multiple cells
+$("#cleanAllCells").click(function()
+{
+  $("td").css("background-color", "");
+});
+
+//Reset Pixel CANVAS_GRID_TABLE
+$("#resetButton").on("click", function(event)
+{
+  CANVAS_GRID_TABLE.empty();
+});
+
+
+//Remove color on doubleclick
+CANVAS_GRID_TABLE.on("dblclick", "td", function()
+{
+  $(this).css("background-color", "");
+});
+
+// Drag and color multiple cells
+CANVAS_GRID_TABLE.on("mousedown", "td", function()
+{
+  mouseDrag = true;
+});
+
+CANVAS_GRID_TABLE.on("mousemove", "td", function()
+{
+  color = $("#colorPicker").val();
+  if (mouseDrag)
+  {
+    $(this).css("background-color", color);
+  }
+});
+
+CANVAS_GRID_TABLE.on("mouseup mouseleave dragstart", function()
+{
+  mouseDrag = false;
+});
